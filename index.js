728016a0c5e184de47964888604147cb0b1340e4
zedeloc <desmondcole1990@gmail.com>
2025-03-11 23:24:53 -0700

    Create basic functions

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello, world!");
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b) {  
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if (a === 0 || b === 0){
+        return "666 FATAL ERROR"
+    }
+    return a / b;
+}
+
+let operandA;
+let operandB;
+let operator;
+
+
+
+// Create a new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
+function operate(a, b, c) {
+    if (c === "+") {
+        return add(a, b);
+    } else if (c === "-") {
+        return subtract(a, b);
+    } else if (c === "*") {
+        return multiply(a, b);
+    } else if (c === "/") {
+        return divide(a ,b);
+    };
+};
