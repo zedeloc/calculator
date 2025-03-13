70a58e074e4613c1b09f1ce7b04eb6c1df0b5d4e
zedeloc <desmondcole1990@gmail.com>
2025-03-12 18:58:02 -0700

    Impliment dual display

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 function add(a, b){
-    return a + b;
+    return +a + +b;
 }
 function subtract(a, b) {  
-    return a - b;
+    return +a - +b;
 }
 function multiply(a, b) {
-    return a * b;
+    return +a * +b;
 }
 function divide(a, b) {
-    if (a === 0 || b === 0){
+    if (+a === 0 || +b === 0){
         return "666 FATAL ERROR"
     }
-    return a / b;
+    return +a / +b;
 }
 
-let operandA;
-let operandB;
-let operator;
+let operandA = "";
+let operandB = "";
+let operator = "";
+const historicValueField = [];
+const activeValueField = []
 
 
 
@@ -32,3 +34,61 @@ function operate(a, b, c) {
         return divide(a ,b);
     };
 };
+
+const displayActive = document.querySelector("#display-active");
+const displayHistoric = document.querySelector('#display-historic')
+const keys = Array.from(document.querySelectorAll(".key"));
+
+
+
+keys.forEach((key) => key.addEventListener("click", () => {
+    if (key.id === 'clear') {
+        displayActive.textContent = ''
+        displayHistoric.textContent = ''
+        operandA = ''
+        operandB = ''
+        operator = ''
+    }else {
+        operatorEval(key.id)
+    }
+}))
+
+function operatorEval(keypress) {
+    if (keypress === "=" && operandA.length > 0) {
+        operandB = displayActive.textContent;
+        let answer = operate(operandA, operandB, operator)
+        displayActive.textContent = answer; 
+    } else if (keypress === '+' 
+        || keypress === '-'
+        || keypress === '*'
+        || keypress === '/') {
+        operator = keypress;
+        displayHistoric.textContent += keypress;
+        operandA = displayActive.textContent;
+        displayActive.textContent = "";
+    } else {
+        displayHistoric.textContent += keypress;
+        displayActive.textContent += keypress;
+    }
+}
+
+function updateValues(valuePassed) {
+
+
+}
+
+function updateValueField(valueField, valuePassed) {
+    if (valuePassed === 'clear') {
+        valueField.length = 0;
+    } else if (valuePassed === 'delete') {
+        valueField.pop();
+    } else if (valuePassed === '.') {
+        for (let value of valueField ) {
+            if (value === ".") { 
+                break;
+            }
+        } 
+    } else {
+        valueField.push(valuePassed)
+    }
+}
